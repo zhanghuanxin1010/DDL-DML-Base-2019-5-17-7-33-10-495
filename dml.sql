abf79ac4a5c6a8ce7d12b19041fd9e4578a014b8
unknown <1012151538@qq.comgit config --global user.name >
2019-08-05 19:38:38 +0800

    dml

--- dml.sql
+++ dml.sql
@@ -1,7 +1,10 @@
--- 插入记录  
-
+﻿-- 插入记录  
+insert into student values('003','王五','18','女')
 -- 修改记录  
+update student set age=19 where id=001
 
 --  删除记录  
+delete from student where id=002
 
 -- 查询记录  
+select* from student
